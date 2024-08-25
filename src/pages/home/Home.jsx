b02f05406600b2b9ab854626469a56a97c65e038
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-08-26 00:35:45 +0600

    Category slider added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Category from './Category';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
-            <h2>This is home</h2>
+            <Category/>
         </div>
     );
 };
