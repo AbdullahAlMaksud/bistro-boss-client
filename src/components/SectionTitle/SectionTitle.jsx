7eff799ee5dd87864025523864dfb6e65bbc9da0
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-09-03 23:54:29 +0600

    popular item has been added

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SectionTitle = ({heading, subheading}) => {
     return (
-        <div>
-            <p>{subheading}</p>
-            <h3>{heading}</h3>
+        <div className='md:w-4/12 my-8 mx-auto text-center'>
+            <p className='text-yellow-600 mb-2'>--- {subheading} ---</p>
+            <h3 className='text-3xl uppercase border border-y-4 py-4'>{heading}</h3>
         </div>
     );
 };
